d1b5fdb94d6aa095d1ad1d06b98c3ee9a8d797a9
inikope <59614495+inikope@users.noreply.github.com>
2020-01-11 00:13:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/', (req, res) => {
         const p1 = instaDown(igid).then(hasil => {
             const { entry_data: { PostPage } } = hasil;
             const data = PostPage.map(post => post.graphql.shortcode_media.edge_sidecar_to_children.edges);
-            console.log(data);
+            console.log(PostPage.map(post => post.graphql.shortcode_media.edge_sidecar_to_children));
             const list = {media: [],preview: []};
 
     		for (let j = 0; j < data.length; j++) {
