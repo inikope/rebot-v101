03614dc800ebb163e5f1e76de6a8f119bcbdd4ed
inikope <59614495+inikope@users.noreply.github.com>
2020-01-11 00:21:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.get('/', (req, res) => {
     	    return {data: list};
         })
         Promise.all([p1]).then(function(values){
-            console.log(values);
+            console.log(values.data.media);
             if(values.data.list.media[number].includes(".mp4")){
                 return client.replyMessage(token, {
                     type: "video", originalContentUrl: values.data.list.media[number], previewImageUrl: values.data.list.preview[number]
